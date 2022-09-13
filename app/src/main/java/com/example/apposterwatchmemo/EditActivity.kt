4457c351951044afa4c24f1e6dcead8da5701fa7
grinleaf <grinleaf774@gmail.com>
2022-09-13 09:58:46 +0900

    init

--- app/src/main/java/com/example/apposterwatchmemo/EditActivity.kt
+++ app/src/main/java/com/example/apposterwatchmemo/EditActivity.kt
@@ -46,40 +46,42 @@ class EditActivity : AppCompatActivity() {
     }
 
     // 모어버튼 메뉴 설정
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            // 내 폰에서 추가
-            R.id.add_my_image -> {
-                // 갤러리 앱 접근 권한 요청
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),200)
-                }else{
-                    val intent = Intent(Intent.ACTION_PICK)
-                    intent.run {
-                        data =  MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                        type = "image/*"
-                    }
-                    registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-                        if(result.resultCode == RESULT_OK){
-                            val img = result.data?.data
-                            Log.i("aaa", "$img")
-                        }
-                    }.launch(intent)
-                }
-            }
-
-            // 페이스 리스트에서 추가
-            R.id.add_face_list -> {
-
-            }
-
-            // 저장하기
-            R.id.save_memo -> {
-
-            }
-
-            // 백버튼 설정
-            android.R.id.home -> onBackPressed()
-        }
-    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            // 내 폰에서 추가
+//            R.id.add_my_image -> {
+//                // 갤러리 앱 접근 권한 요청
+//                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+//                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),200)
+//                }else{
+//                    val intent = Intent(Intent.ACTION_PICK)
+//                    intent.run {
+//                        data =  MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+//                        type = "image/*"
+//                    }
+//                    registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+//                        if(result.resultCode == RESULT_OK){
+//                            val img = result.data?.data
+//                            Log.i("aaa", "$img")
+//                        }
+//                    }.launch(intent)
+//                }
+//            }
+//
+//            // 페이스 리스트에서 추가
+//            R.id.add_face_list -> {
+//
+//            }
+//
+//            // 저장하기
+//            R.id.save_memo -> {
+//
+//            }
+//
+//            // 백버튼 설정
+//            android.R.id.home -> {
+//                onBackPressed()
+//            }
+//        }
+//    }
 }
