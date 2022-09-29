74bbc7e7c96d0b7a0aa3b1d7f1f8837a2fa174fe
grinleaf <grinleaf774@gmail.com>
2022-09-29 18:32:43 +0900

    9/29 end

--- app/src/main/java/com/example/apposterwatchmemo/MainActivity.kt
+++ app/src/main/java/com/example/apposterwatchmemo/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.apposterwatchmemo
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,8 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.viewModels
-import androidx.lifecycle.Observer
+import androidx.core.view.setPadding
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -18,7 +16,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.example.apposterwatchmemo.databinding.ActivityMainBinding
-import okhttp3.internal.notify
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -42,18 +39,28 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
+
+        // * AdapterDataObserver : 어댑터 내의 데이터 변동을 감지하는 옵저버 추상 클래스. 어댑터 내의 아이템 포지션 값을 가져와 이벤트 처리하기 좋을 것 같음. 해당 메소드 외에 값 추가/제거/범위 변경 시점에 대한 이벤트 처리 및 제어도 가능
+//        adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver(){
+//            override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+//                super.onItemRangeRemoved(positionStart, itemCount)
+//                adapter.submitList(viewModel.mainListLiveData.value)
+//            }
+//        })
+
         binding.recycler.adapter = adapter
+
 
         // 플로팅버튼 클릭 시 편집화면으로 전환
         binding.fab.setOnClickListener { startActivity(Intent(this@MainActivity, EditActivity::class.java)) }
 
         viewModel.mainListLiveData.observe(this@MainActivity){
-            adapter.submitList(it)
-            setUiVisibility()
+            setListToUi(it)
         }
     }
 
-    private fun setUiVisibility(){
+    fun setListToUi(liveData: List<MainListModel>){
+        adapter.submitList(liveData)
         if(adapter.itemCount == 0){
             binding.noItemMain.visibility = View.VISIBLE
             binding.recycler.visibility = View.GONE
@@ -76,16 +83,17 @@ class MainListAdapter(private val requestManager: RequestManager,val onClick: (I
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        requestManager.load(getItem(position).imgUrl).into(holder.img)
-        if (getItem(position).imgUrl.contains("http")) holder.img.setPadding(16, 16, 16, 16)
-        holder.title.text = getItem(position).title
-        holder.content.text = getItem(position).content
+        val item = getItem(position)
+        requestManager.load(item.imgUrl).into(holder.img)
+        if (item.imgUrl.contains("http")) holder.img.setPadding(16)
+        holder.title.text = item.title
+        holder.content.text = item.content
 
         holder.itemView.setOnClickListener {
 
             // * error : 처음 혹은 중간에 위치한 아이템이 delete 된 후, position 이 갱신되지 않은 상태로 리스너가 추가되므로 IndexOutOfBoundsException 발생
 
-            // 1. 단순 분기처리로 할수 없음.....position 이 꼬이는뎅
+            // 1. 단순히 position 값만 바꿀 수 없음.....position 이 꼬이는뎅 --> 수식 만들면 가능하려남?
 //            if(position==itemCount) {
 //                onClick.invoke(
 //                    getItem(position-1).id,
@@ -95,24 +103,23 @@ class MainListAdapter(private val requestManager: RequestManager,val onClick: (I
 //                )
 //                return@setOnClickListener
 //            }else onClick.invoke(
-//                getItem(position).id,
-//                getItem(position).imgUrl,
-//                getItem(position).title!!,
-//                getItem(position).content!!
+//                item.id,
+//                item.imgUrl,
+//                item.title!!,
+//                item.content!!
 //            )
 
             // 2. 어댑터 갱신. 두 번 클릭해야 DetailAc 으로 넘어감.. return 빼면 오류
-            if (position == itemCount) {
-                notifyDataSetChanged()
-                return@setOnClickListener
-            }else{
-                onClick.invoke(
-                    getItem(position).id,
-                    getItem(position).imgUrl,
-                    getItem(position).title!!,
-                    getItem(position).content!!
-                )
-            }
+//            if (position == itemCount) {
+//                notifyDataSetChanged()
+//                return@setOnClickListener
+//            }
+            onClick.invoke(
+                item.id,
+                item.imgUrl,
+                item.title!!,
+                item.content!!,
+            )
         }
     }
 
